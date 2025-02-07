4897aec00d2516c57b8782b1018b1023287f8722
Stjepan Svib <svib.stjepan@gmail.com>
2025-02-07 16:07:15 +0100

    finished IF statements

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ document.getElementById("mySubmit").onclick = function(){
 
 //COUNTER PROGRAM
 
-const decreaseBtn = document.getElementById("decreaseBtn");
+/*const decreaseBtn = document.getElementById("decreaseBtn");
 const resetBtn = document.getElementById("resetBtn");
 const increaseBtn = document.getElementById("increaseBtn");
 const countLabel = document.getElementById("countLabel");
@@ -138,6 +138,97 @@ resetBtn.onclick = function(){
     count = 0;
     countLabel.textContent = count;
 }
+*/
+
+//math 
+// = built in object that provides a collection of properties and methods
+
+//let x = 3;
+//let y = 2;
+//let z = 1;
+
+//z = Math.round(x); rounds a number
+//z = Math.floor(x); rounds down
+//z = Math.ceil(x)   rounds up
+//z = Math.trunc(x);  eliminates decimals
+// z = Math.pow(x,y); on power of ...
+//z = Math.sqrt(x);
+//z = Math.log(x);
+//z = Math.sin(x);
+//z = Math.cos(x);
+//z = Math.tan(x);
+//z = Math.abs(x); absolute value
+// z = Math.sign(x);
+//let max = Math.max(x, y, z);
+//let min = Math.min(x, y, z);
+//console.log(min);
+
+
+// RANDOM NUMBER GENERATOR
+
+/*const min = 50;
+const max = 100;
+let randomNum = Math.floor(Math.random() * (max-min)) + min;
+console.log(randomNum);
+*/
+
+/*const myButton = document.getElementById("myButton");
+const Label1 = document.getElementById("Label1");
+const Label2 = document.getElementById("Label2");
+const Label3 = document.getElementById("Label3");
+
+const min = 1;
+const max = 6;
+
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+myButton.onclick = function(){
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    Label1.textContent = randomNum1;
+    Label2.textContent = randomNum2;
+    Label3.textContent = randomNum3;
+}
+*/
+
+// IF STATEMENTS
+
+// if a condition is true , execute code
+// if not , do something else
+/*const myText = document.getElementById("myText");
+const mySubmit = document.getElementById("mySubmit");
+const resultElement = document.getElementById("resultElement");
+let age;
+
+mySubmit.onclick = function(){
+
+    age = myText.value;
+    age=Number(age);
+
+
+    if (age >=100){
+        resultElement.textContent = `You are  TOO OLD to enter this site`
+    }
+    else if (age == 0){
+        resultElement.textContent = `You cant enter you were just born`
+    }
+    else if (age >= 18){
+        resultElement.textContent = `you are old engouh to enter this site`
+    }
+    else if(age<0){
+        resultElement.textContent = `Your age cant be below 0`
+    }
+    else if (age >=100){
+        resultElement.textContent = `You are  TOO OLD to enter this site`
+    }
+    else{
+         resultElement.textContent = `You must be 18+ to enter this site`
+    }
+}
+*/
 
 
 
